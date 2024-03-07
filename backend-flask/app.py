f9a0c22605b2728de13763ca4fa0b43f22294114
Hannan Abid <abidh0601@gmail.com>
2024-03-07 03:37:59 +0000

    Add aws xray instrumentation

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,6 +22,10 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+#x-ray
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 # HoneyComb
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -29,6 +33,10 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
+
+#Xray initialize
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 
 app = Flask(__name__)
@@ -47,6 +55,9 @@ cors = CORS(
 #Initialize Automatic Instrumentation with Flask - Honeycomb
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument
+
+#Xray initialize
+XRayMiddleware(app, xray_recorder)
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
