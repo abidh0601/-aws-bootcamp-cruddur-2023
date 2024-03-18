cbc75f663100bc80634841949fa1e28a3bbb4776
Hannan Abid <abidh0601@gmail.com>
2024-03-18 02:03:38 +0000

    SiginPage changes and gitpod.yml updates

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -3,24 +3,33 @@ import React from "react";
 import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
 
-// [TODO] Authenication
-import Cookies from 'js-cookie'
+import { Auth } from 'aws-amplify'; 
+
 
 export default function SigninPage() {
 
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
-  const [errors, setErrors] = React.useState('');
+  const [cognitoErrors, setCognitoErrors]  = React.useState('')
 
   const onsubmit = async (event) => {
     event.preventDefault();
-    setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
-      window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
+    setCognitoErrors('')
+    try {
+      Auth.signIn(email, password)
+        .then(user => {
+            localStorage.setItem("access-token", user.sigInUserSession.accessToken.jwtToken)
+          }
+        )
+        .catch(err => {
+          console.log('Error!', err)
+          setCognitoErrors(err.message)
+        })
+    } catch (error) {
+      if (error.code == 'UserNotConfirmedException') {
+        window.location.href = "/confirm"
+      }
+      setCognitoErrors(error.message)
     }
     return false
   }
@@ -32,9 +41,9 @@ export default function SigninPage() {
     setPassword(event.target.value);
   }
 
-  let el_errors;
-  if (errors){
-    el_errors = <div className='errors'>{errors}</div>;
+  let errors;
+  if (cognitoErrors){
+    errors = <div className='errors'>{cognitoErrors}</div>;
   }
 
   return (
@@ -43,6 +52,7 @@ export default function SigninPage() {
         <Logo className='logo' />
       </div>
       <div className='signin-wrapper'>
+        
         <form 
           className='signin_form'
           onSubmit={onsubmit}
@@ -66,7 +76,7 @@ export default function SigninPage() {
               />
             </div>
           </div>
-          {el_errors}
+          {errors}
           <div className='submit'>
             <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
             <button type='submit'>Sign In</button>
