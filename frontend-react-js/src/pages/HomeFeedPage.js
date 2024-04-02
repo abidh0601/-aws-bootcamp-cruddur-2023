4754ccfe59c76dc0f8b4c46ecdb21d45c4fa3247
abidh0601 <38389544+abidh0601@users.noreply.github.com>
2024-04-01 19:35:58 -0500

    Week 3 - Cognito / JWT
    * Add AWS Amplify integration for cognito
    
    * SiginPage changes and gitpod.yml updates
    
    * fix signin onSubmit function
    
    * fix redirect when logging in
    
    * Add signup and account recovery
    
    * comment out unneeded containers
    
    * add debug argument for flask
    
    * add jwt verification on backend
    
    * remove access-token from local storage on logout
    
    * set authorization token in header when getting data for activities
    
    ---------
    
    Co-authored-by: Hannan Abid <hannan@letsusedata.com>

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -10,6 +10,8 @@ import ReplyForm from '../components/ReplyForm';
 // [TODO] Authenication
 import Cookies from 'js-cookie'
 
+import { Auth } from 'aws-amplify';
+
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
   const [popped, setPopped] = React.useState(false);
@@ -22,7 +24,10 @@ export default function HomeFeedPage() {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
       const res = await fetch(backend_url, {
-        method: "GET"
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access-token")}`
+        }
       });
       let resJson = await res.json();
       if (res.status === 200) {
@@ -36,14 +41,20 @@ export default function HomeFeedPage() {
   };
 
   const checkAuth = async () => {
-    console.log('checkAuth')
-    // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
+    Auth.currentAuthenticatedUser({
+      bypassCache: false
+    })
+    .then((user) => {
+      console.log('user', user)
+      return Auth.currentAuthenticatedUser()
+    })
+    .then((cognito_user) => {
       setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
+        display_name: cognito_user.attributes.name,
+        handle: cognito_user.attributes.preferred_username
       })
-    }
+    })
+    .catch((err) => console.log(err))
   };
 
   React.useEffect(()=>{
--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -3,38 +3,45 @@ import React from "react";
 import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
 
-// [TODO] Authenication
-import Cookies from 'js-cookie'
+import { Auth } from 'aws-amplify'; 
+
 
 export default function SigninPage() {
 
-  const [email, setEmail] = React.useState('');
+  const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
-  const [errors, setErrors] = React.useState('');
+  const [cognitoErrors, setCognitoErrors]  = React.useState('')
 
   const onsubmit = async (event) => {
     event.preventDefault();
-    setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
-      window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
-    }
+    setCognitoErrors('')
+
+    Auth.signIn(username, password)
+      .then(user => {
+          localStorage.setItem("access-token", user.signInUserSession.accessToken.jwtToken)
+          window.location.href = "/"
+        }
+      )
+      .catch(error => {
+        if (error.code == 'UserNotConfirmedException') {
+          window.location.href = "/confirm"
+        }
+        setCognitoErrors(error.message)
+      })
+  
     return false
   }
 
-  const email_onchange = (event) => {
-    setEmail(event.target.value);
+  const username_onchange = (event) => {
+    setUsername(event.target.value);
   }
   const password_onchange = (event) => {
     setPassword(event.target.value);
   }
 
-  let el_errors;
-  if (errors){
-    el_errors = <div className='errors'>{errors}</div>;
+  let errors;
+  if (cognitoErrors){
+    errors = <div className='errors'>{cognitoErrors}</div>;
   }
 
   return (
@@ -43,6 +50,7 @@ export default function SigninPage() {
         <Logo className='logo' />
       </div>
       <div className='signin-wrapper'>
+        
         <form 
           className='signin_form'
           onSubmit={onsubmit}
@@ -50,11 +58,11 @@ export default function SigninPage() {
           <h2>Sign into your Cruddur account</h2>
           <div className='fields'>
             <div className='field text_field username'>
-              <label>Email</label>
+              <label>Username or Email</label>
               <input
                 type="text"
-                value={email}
-                onChange={email_onchange} 
+                value={username}
+                onChange={username_onchange} 
               />
             </div>
             <div className='field text_field password'>
@@ -66,7 +74,7 @@ export default function SigninPage() {
               />
             </div>
           </div>
-          {el_errors}
+          {errors}
           <div className='submit'>
             <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
             <button type='submit'>Sign In</button>
